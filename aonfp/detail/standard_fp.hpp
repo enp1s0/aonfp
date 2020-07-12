3b9c6d9f8f255d83105cf2b37951077dc78da1da
mutsuki <mutsuki@momo86.net>
2020-07-12 23:51:46 +0900

    Add `get_mantissa_size`

--- aonfp/detail/standard_fp.hpp
+++ aonfp/detail/standard_fp.hpp
@@ -9,6 +9,11 @@ template <class T>
 constexpr unsigned get_exponent_size();
 template <> constexpr unsigned get_exponent_size<float >() {return 8 ;}
 template <> constexpr unsigned get_exponent_size<double>() {return 11;}
+
+template <class T>
+constexpr unsigned get_mantissa_size();
+template <> constexpr unsigned get_mantissa_size<float >() {return 23;}
+template <> constexpr unsigned get_mantissa_size<double>() {return 52;}
 }
 } // namespace aonfp
 #endif
