87b5c88518cc7ffefd99aa7a2e282f4eee472ba8
mutsuki <mutsuki@momo86.net>
2020-07-12 23:50:56 +0900

    Add `get_exponent_size`

---
+++ aonfp/detail/standard_fp.hpp
@@ -0,0 +1,14 @@
+#ifndef __AONFP_DETAIL_STANDARDFP_HPP__
+#define __AONFP_DETAIL_STANDARDFP_HPP__
+#include <cstdint>
+
+namespace aonfp {
+namespace detail {
+// If all people in the world used C++14 or later, these functions could be `template constexpr`.
+template <class T>
+constexpr unsigned get_exponent_size();
+template <> constexpr unsigned get_exponent_size<float >() {return 8 ;}
+template <> constexpr unsigned get_exponent_size<double>() {return 11;}
+}
+} // namespace aonfp
+#endif
