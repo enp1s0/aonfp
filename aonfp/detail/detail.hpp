b7ee8468dc9eddfb48c8ac61ea570e526a3ab870
mutsuki <mutsuki@momo86.net>
2020-07-13 13:06:11 +0900

    Add `set_inf_mantissa`

--- aonfp/detail/detail.hpp
+++ aonfp/detail/detail.hpp
@@ -30,6 +30,9 @@ template <> constexpr void set_inf_exponent<uint32_t>(uint32_t& v) {v = 0x7fffff
 template <> constexpr void set_inf_exponent<uint16_t>(uint16_t& v) {v = 0x7fff | (v & 0x8000);}
 template <> constexpr void set_inf_exponent<uint8_t >(uint8_t & v) {v = 0x7f | (v & 0x80);}
 
+template <class T>
+constexpr void set_inf_mantissa(T& v) {v = 0;};
+
 } //namespace detail
 } //namespace aonfp
 #endif
