96647f652b7cbea439c630723e7260c1d744d5ad
mutsuki <mutsuki@momo86.net>
2020-07-13 13:27:03 +0900

    Add `copy_mantissa`

--- aonfp/detail/detail.hpp
+++ aonfp/detail/detail.hpp
@@ -33,6 +33,33 @@ template <> constexpr void set_inf_exponent<uint8_t >(uint8_t & v) {v = 0x7f | (
 template <class T>
 constexpr void set_inf_mantissa(T& v) {v = 0;};
 
+template <class T>
+inline void copy_mantissa(T& mantissa, const double v);
+template <> inline void copy_mantissa<uint64_t>(uint64_t& mantissa, const double v) {
+	mantissa = (*reinterpret_cast<const uint64_t*>(&v)) << 12;
+}
+template <> inline void copy_mantissa<uint32_t>(uint32_t& mantissa, const double v) {
+	const auto mantissa_bs = (*reinterpret_cast<const uint32_t*>(&v)) & 0xffffffffffffflu;
+	const auto r_s = (mantissa_bs & 0x80000lu) << 1;
+	const auto mantissa_bs_a = mantissa_bs + r_s;
+	const auto digit_up = (mantissa_bs_a & 0x10000000000000lu) >> 52;
+	mantissa = mantissa_bs_a >> (20 + digit_up);
+}
+template <> inline void copy_mantissa<uint16_t>(uint16_t& mantissa, const double v) {
+	const auto mantissa_bs = (*reinterpret_cast<const uint32_t*>(&v)) & 0xffffffffffffflu;
+	const auto r_s = (mantissa_bs & 0x800000000lu) << 1;
+	const auto mantissa_bs_a = mantissa_bs + r_s;
+	const auto digit_up = (mantissa_bs_a & 0x10000000000000lu) >> 52;
+	mantissa = mantissa_bs_a >> (36 + digit_up);
+}
+template <> inline void copy_mantissa<uint8_t>(uint8_t& mantissa, const double v) {
+	const auto mantissa_bs = (*reinterpret_cast<const uint32_t*>(&v)) & 0xffffffffffffflu;
+	const auto r_s = (mantissa_bs & 0x80000000000lu) << 1;
+	const auto mantissa_bs_a = mantissa_bs + r_s;
+	const auto digit_up = (mantissa_bs_a & 0x10000000000000lu) >> 52;
+	mantissa = mantissa_bs_a >> (44 + digit_up);
+}
+
 } //namespace detail
 } //namespace aonfp
 #endif
