1805d795ff51cc14567fe07eaaea274ad7217a2b
mutsuki <mutsuki@momo86.net>
2020-07-17 11:01:34 +0900

    Add namespace `standard_fp`

--- aonfp/detail/standard_fp.hpp
+++ aonfp/detail/standard_fp.hpp
@@ -4,6 +4,7 @@
 
 namespace aonfp {
 namespace detail {
+namespace standard_fp {
 // If all people in the world used C++14 or later, these functions could be `template constexpr`.
 template <class T>
 constexpr unsigned get_exponent_size();
@@ -14,6 +15,7 @@ template <class T>
 constexpr unsigned get_mantissa_size();
 template <> constexpr unsigned get_mantissa_size<float >() {return 23;}
 template <> constexpr unsigned get_mantissa_size<double>() {return 52;}
+} // namespace standard_fp
 } // namespace detail
 } // namespace aonfp
 #endif
