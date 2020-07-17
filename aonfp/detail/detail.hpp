c8495afbdf13051761636a7d7754ace582ea1ae2
mutsuki <mutsuki@momo86.net>
2020-07-17 21:22:33 +0900

    Fix `decompose_sign_exponent`

--- aonfp/detail/detail.hpp
+++ aonfp/detail/detail.hpp
@@ -129,9 +129,9 @@ inline T decompose_sign_exponent(const double v, const int move_up, uo_flow_t& u
 
 template <class T>
 inline T decompose_sign_exponent(const float v, const int move_up, uo_flow_t& uo) {
-	const auto sign = (*reinterpret_cast<const uint32_t*>(&v)) & 0x80000000lu;
-	const auto exponent = (((*reinterpret_cast<const uint32_t*>(&v)) & 0x7f800000lu) >> 23) + move_up;
-	const auto res_s = static_cast<T>((sign & 0x80000000lu) >> (32 - 8 * sizeof(T)));
+	const auto sign = ((*reinterpret_cast<const uint32_t*>(&v)) & 0x80000000) >> 31;
+	const auto exponent = (((*reinterpret_cast<const uint32_t*>(&v)) & 0x7f800000) >> 23) + move_up;
+	const auto res_s = static_cast<T>(sign) << (sizeof(T) * 8 - 1);
 	const auto src_exponent = static_cast<typename std::make_signed<T>::type>(exponent) - detail::get_default_exponent_bias(8);
 	const auto dst_exponent = static_cast<T>(src_exponent + detail::get_default_exponent_bias(sizeof(T) * 8 - 1));
 	if (dst_exponent >> (sizeof(T) * 8 - 1)) {
