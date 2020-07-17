ddf8382fcaa97d9d16ba3628ed8c9b85aa279de3
mutsuki <mutsuki@momo86.net>
2020-07-17 10:50:38 +0900

    Rename calling functions

--- aonfp/aonfp.hpp
+++ aonfp/aonfp.hpp
@@ -5,21 +5,21 @@
 
 namespace aonfp {
 template <class T, class S_EXP_T, class MANTISSA_T>
-inline void convert(S_EXP_T& s_exp, MANTISSA_T& mantissa, const T v) {
-	int digit_up;
-	mantissa = detail::copy_mantissa<MANTISSA_T>(v, digit_up);
+inline void decompose(S_EXP_T& s_exp, MANTISSA_T& mantissa, const T v) {
+	int move_up;
+	mantissa = detail::decompose_mantissa<MANTISSA_T>(v, move_up);
 	detail::uo_flow_t uo;
-	s_exp = detail::copy_sign_exponent<S_EXP_T>(v, digit_up, uo);
+	s_exp = detail::decompose_sign_exponent<S_EXP_T>(v, move_up, uo);
 
 	if (uo == detail::uo_flow_overflow) {
-		s_exp = detail::get_inf_exponent<S_EXP_T>(0);
-		mantissa = detail::get_inf_mantissa<MANTISSA_T>();
+		s_exp = detail::get_inf_sign_exponent_bitstring<S_EXP_T>(s_exp);
+		mantissa = detail::get_inf_mantissa_bitstring<MANTISSA_T>();
 		return;
 	}
 
 	if (uo == detail::uo_flow_underflow) {
-		s_exp = detail::get_inf_exponent<S_EXP_T>(s_exp);
-		mantissa = detail::get_inf_mantissa<MANTISSA_T>();
+		s_exp = detail::get_zero_sign_exponent_bitstring<S_EXP_T>(s_exp);
+		mantissa = detail::get_zero_mantissa_bitstring<MANTISSA_T>();
 		return;
 	}
 }
