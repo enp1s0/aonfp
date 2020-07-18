03d3a45c32cbc08e21cabbab8e72846595c79ec4
mutsuki <mutsuki@momo86.net>
2020-07-18 15:28:27 +0900

    Add `AONFP_HOST_DEVICE`

--- include/aonfp/detail/detail.hpp
+++ include/aonfp/detail/detail.hpp
@@ -1,5 +1,13 @@
 #ifndef __AONFP_DETAIL_DETAIL_HPP__
 #define __AONFP_DETAIL_DETAIL_HPP__
+
+#ifndef AONFP_HOST_DEVICE
+ #if defined(__CUDA_ARCH__)
+  #define AONFP_HOST_DEVICE __device__ __host__
+ #else
+  #define AONFP_HOST_DEVICE
+ #endif
+#endif
 #include <limits>
 #include <cstdint>
 #include <type_traits>
@@ -21,49 +29,49 @@ template <> struct bitstring_t<double> {using type = uint64_t;};
 template <> struct bitstring_t<const float > {using type = uint32_t;};
 template <> struct bitstring_t<const double> {using type = uint64_t;};
 
-constexpr unsigned long get_default_exponent_bias(const unsigned exponent_size) {
+AONFP_HOST_DEVICE constexpr unsigned long get_default_exponent_bias(const unsigned exponent_size) {
 	return (1 << (exponent_size - 1)) - 1;
 }
 
 template <class T>
-constexpr T get_exponent_bitstring(const T s_exp) {return ((static_cast<T>(1) << (sizeof(T) * 8 - 1)) - 1) & s_exp;}
+AONFP_HOST_DEVICE constexpr T get_exponent_bitstring(const T s_exp) {return ((static_cast<T>(1) << (sizeof(T) * 8 - 1)) - 1) & s_exp;}
 
 template <class T>
-constexpr T get_sign_bitstring(const T s_exp) {return s_exp & (static_cast<T>(1) << (sizeof(T) * 8 - 1));}
+AONFP_HOST_DEVICE constexpr T get_sign_bitstring(const T s_exp) {return s_exp & (static_cast<T>(1) << (sizeof(T) * 8 - 1));}
 
 template <class T>
-constexpr T get_nan_sign_exponent_bitstring() {return (static_cast<T>(1) << (sizeof(T) * 8 - 1)) - 1;}
+AONFP_HOST_DEVICE constexpr T get_nan_sign_exponent_bitstring() {return (static_cast<T>(1) << (sizeof(T) * 8 - 1)) - 1;}
 
 template <class T>
-constexpr T get_nan_mantissa_bitstring() {return ~static_cast<T>(0);}
+AONFP_HOST_DEVICE constexpr T get_nan_mantissa_bitstring() {return ~static_cast<T>(0);}
 
 template <class T>
-constexpr T get_inf_sign_exponent_bitstring(T s_exp) {return get_nan_sign_exponent_bitstring<T>() | get_sign_bitstring(s_exp);}
+AONFP_HOST_DEVICE constexpr T get_inf_sign_exponent_bitstring(T s_exp) {return get_nan_sign_exponent_bitstring<T>() | get_sign_bitstring(s_exp);}
 
 template <class T>
-constexpr T get_inf_mantissa_bitstring() {return static_cast<T>(0);};
+AONFP_HOST_DEVICE constexpr T get_inf_mantissa_bitstring() {return static_cast<T>(0);};
 
 template <class T>
-constexpr T get_zero_sign_exponent_bitstring(T s_exp = 0) {return get_sign_bitstring(s_exp);}
+AONFP_HOST_DEVICE constexpr T get_zero_sign_exponent_bitstring(T s_exp = 0) {return get_sign_bitstring(s_exp);}
 
 template <class T>
-constexpr T get_zero_mantissa_bitstring() {return static_cast<T>(0);};
+AONFP_HOST_DEVICE constexpr T get_zero_mantissa_bitstring() {return static_cast<T>(0);};
 
 
 // range getter
-constexpr long get_max_exponent(const unsigned expopent_length) {return static_cast<long>(get_default_exponent_bias(expopent_length));}
+AONFP_HOST_DEVICE constexpr long get_max_exponent(const unsigned expopent_length) {return static_cast<long>(get_default_exponent_bias(expopent_length));}
 
-constexpr long get_min_exponent(const unsigned exponent_length) {return 1 - static_cast<long>(get_default_exponent_bias(exponent_length));}
+AONFP_HOST_DEVICE constexpr long get_min_exponent(const unsigned exponent_length) {return 1 - static_cast<long>(get_default_exponent_bias(exponent_length));}
 
 // composer / decomposer
 template <class T>
-inline T decompose_mantissa(const double v, int& move_up);
-template <> inline uint64_t decompose_mantissa<uint64_t>(const double v, int& move_up) {
+AONFP_HOST_DEVICE inline T decompose_mantissa(const double v, int& move_up);
+template <> AONFP_HOST_DEVICE inline uint64_t decompose_mantissa<uint64_t>(const double v, int& move_up) {
 	move_up = 0;
 	return (*reinterpret_cast<const uint64_t*>(&v)) << 12;
 }
 
-template <> inline uint32_t decompose_mantissa<uint32_t>(const double v, int& move_up) {
+template <> AONFP_HOST_DEVICE inline uint32_t decompose_mantissa<uint32_t>(const double v, int& move_up) {
 	const auto mantissa_bs = (*reinterpret_cast<const uint64_t*>(&v)) & 0xffffffffffffflu;
 	const auto r_s = (mantissa_bs & 0x80000lu) << 1;
 	const auto mantissa_bs_a = mantissa_bs + r_s;
@@ -71,7 +79,7 @@ template <> inline uint32_t decompose_mantissa<uint32_t>(const double v, int& mo
 	return mantissa_bs_a >> 20;
 }
 
-template <> inline uint16_t decompose_mantissa<uint16_t>(const double v, int& move_up) {
+template <> AONFP_HOST_DEVICE inline uint16_t decompose_mantissa<uint16_t>(const double v, int& move_up) {
 	const auto mantissa_bs = (*reinterpret_cast<const uint64_t*>(&v)) & 0xffffffffffffflu;
 	const auto r_s = (mantissa_bs & 0x800000000lu) << 1;
 	const auto mantissa_bs_a = mantissa_bs + r_s;
@@ -79,7 +87,7 @@ template <> inline uint16_t decompose_mantissa<uint16_t>(const double v, int& mo
 	return mantissa_bs_a >> 36;
 }
 
-template <> inline uint8_t decompose_mantissa<uint8_t>(const double v, int& move_up) {
+template <> AONFP_HOST_DEVICE inline uint8_t decompose_mantissa<uint8_t>(const double v, int& move_up) {
 	const auto mantissa_bs = (*reinterpret_cast<const uint64_t*>(&v)) & 0xffffffffffffflu;
 	const auto r_s = (mantissa_bs & 0x80000000000lu) << 1;
 	const auto mantissa_bs_a = mantissa_bs + r_s;
@@ -88,18 +96,18 @@ template <> inline uint8_t decompose_mantissa<uint8_t>(const double v, int& move
 }
 
 template <class T>
-inline T decompose_mantissa(const float v, int& move_up);
-template <> inline uint64_t decompose_mantissa<uint64_t>(const float v, int& move_up) {
+AONFP_HOST_DEVICE inline T decompose_mantissa(const float v, int& move_up);
+template <> AONFP_HOST_DEVICE inline uint64_t decompose_mantissa<uint64_t>(const float v, int& move_up) {
 	move_up = 0;
 	return (*reinterpret_cast<const uint64_t*>(&v)) << (9 + 32);
 }
 
-template <> inline uint32_t decompose_mantissa<uint32_t>(const float v, int& move_up) {
+template <> AONFP_HOST_DEVICE inline uint32_t decompose_mantissa<uint32_t>(const float v, int& move_up) {
 	move_up = 0;
 	return (*reinterpret_cast<const uint32_t*>(&v)) << 9;
 }
 
-template <> inline uint16_t decompose_mantissa<uint16_t>(const float v, int& move_up) {
+template <> AONFP_HOST_DEVICE inline uint16_t decompose_mantissa<uint16_t>(const float v, int& move_up) {
 	const auto mantissa_bs = (*reinterpret_cast<const uint32_t*>(&v)) & 0x7fffff;
 	const auto r_s = (mantissa_bs & 0x40) << 1;
 	const auto mantissa_bs_a = mantissa_bs + r_s;
@@ -107,7 +115,7 @@ template <> inline uint16_t decompose_mantissa<uint16_t>(const float v, int& mov
 	return mantissa_bs_a >> 7;
 }
 
-template <> inline uint8_t decompose_mantissa<uint8_t>(const float v, int& move_up) {
+template <> AONFP_HOST_DEVICE inline uint8_t decompose_mantissa<uint8_t>(const float v, int& move_up) {
 	const auto mantissa_bs = (*reinterpret_cast<const uint32_t*>(&v)) & 0x7fffff;
 	const auto r_s = (mantissa_bs & 0x8000) << 1;
 	const auto mantissa_bs_a = mantissa_bs + r_s;
@@ -116,7 +124,7 @@ template <> inline uint8_t decompose_mantissa<uint8_t>(const float v, int& move_
 }
 
 template <class T>
-inline T decompose_sign_exponent(const double v, const int move_up, uo_flow_t& uo) {
+AONFP_HOST_DEVICE inline T decompose_sign_exponent(const double v, const int move_up, uo_flow_t& uo) {
 	const auto sign = (*reinterpret_cast<const uint64_t*>(&v)) & 0x8000000000000000lu;
 	const auto exponent = (((*reinterpret_cast<const uint64_t*>(&v)) & 0x7ff0000000000000lu) >> 52) + move_up;
 	const auto res_s = static_cast<T>((sign & 0x8000000000000000lu) >> (64 - 8 * sizeof(T)));
@@ -137,7 +145,7 @@ inline T decompose_sign_exponent(const double v, const int move_up, uo_flow_t& u
 }
 
 template <class T>
-inline T decompose_sign_exponent(const float v, const int move_up, uo_flow_t& uo) {
+AONFP_HOST_DEVICE inline T decompose_sign_exponent(const float v, const int move_up, uo_flow_t& uo) {
 	const auto sign = ((*reinterpret_cast<const uint32_t*>(&v)) & 0x80000000) >> 31;
 	const auto exponent = (((*reinterpret_cast<const uint32_t*>(&v)) & 0x7f800000) >> 23) + move_up;
 	const auto res_s = static_cast<T>(sign) << (sizeof(T) * 8 - 1);
@@ -158,7 +166,7 @@ inline T decompose_sign_exponent(const float v, const int move_up, uo_flow_t& uo
 }
 
 template <class T, class MANTISSA_T>
-inline T compose_mantissa(const MANTISSA_T mantissa, const T src_fp, int& move_up) {
+AONFP_HOST_DEVICE inline T compose_mantissa(const MANTISSA_T mantissa, const T src_fp, int& move_up) {
 	const auto shifted_m = mantissa >> (sizeof(MANTISSA_T) * 8 - standard_fp::get_mantissa_size<T>());
 	const auto s = (mantissa >> (sizeof(MANTISSA_T) * 8 - standard_fp::get_mantissa_size<T>() - 1) & 0x1);
 	const auto shifted_m_a = shifted_m + s;
@@ -167,35 +175,35 @@ inline T compose_mantissa(const MANTISSA_T mantissa, const T src_fp, int& move_u
 	return *reinterpret_cast<const T*>(&full_bitstring);
 }
 
-template <> inline double compose_mantissa<double, uint32_t>(const uint32_t m, const double src_fp, int& move_up) {
+template <> AONFP_HOST_DEVICE inline double compose_mantissa<double, uint32_t>(const uint32_t m, const double src_fp, int& move_up) {
 	const auto shifted_m = static_cast<uint64_t>(m) << 20;
 	move_up = 0;
 	const auto full_bitstring = shifted_m | (*reinterpret_cast<const uint64_t*>(&src_fp) & 0xfff0000000000000lu);
 	return *reinterpret_cast<const double*>(&full_bitstring);
 }
 
-template <> inline double compose_mantissa<double, uint16_t>(const uint16_t m, const double src_fp, int& move_up) {
+template <> AONFP_HOST_DEVICE inline double compose_mantissa<double, uint16_t>(const uint16_t m, const double src_fp, int& move_up) {
 	const auto shifted_m = static_cast<uint64_t>(m) << 36;
 	move_up = 0;
 	const auto full_bitstring = shifted_m | (*reinterpret_cast<const uint64_t*>(&src_fp) & 0xfff0000000000000lu);
 	return *reinterpret_cast<const double*>(&full_bitstring);
 }
 
-template <> inline double compose_mantissa<double, uint8_t>(const uint8_t m, const double src_fp, int& move_up) {
+template <> AONFP_HOST_DEVICE inline double compose_mantissa<double, uint8_t>(const uint8_t m, const double src_fp, int& move_up) {
 	const auto shifted_m = static_cast<uint64_t>(m) << 44;
 	move_up = 0;
 	const auto full_bitstring = shifted_m | (*reinterpret_cast<const uint64_t*>(&src_fp) & 0xfff0000000000000lu);
 	return *reinterpret_cast<const double*>(&full_bitstring);
 }
 
-template <> inline float compose_mantissa<float , uint16_t>(const uint16_t m, const float src_fp, int& move_up) {
+template <> AONFP_HOST_DEVICE inline float compose_mantissa<float , uint16_t>(const uint16_t m, const float src_fp, int& move_up) {
 	const auto shifted_m = static_cast<uint32_t>(m) << 7;
 	move_up = 0;
 	const auto full_bitstring = shifted_m | (*reinterpret_cast<const uint32_t*>(&src_fp) & 0xff800000);
 	return *reinterpret_cast<const float*>(&full_bitstring);
 }
 
-template <> inline float compose_mantissa<float , uint8_t>(const uint8_t m, const float src_fp, int& move_up) {
+template <> AONFP_HOST_DEVICE inline float compose_mantissa<float , uint8_t>(const uint8_t m, const float src_fp, int& move_up) {
 	const auto shifted_m = static_cast<uint32_t>(m) << 15;
 	move_up = 0;
 	const auto full_bitstring = shifted_m | (*reinterpret_cast<const uint32_t*>(&src_fp) & 0xff800000);
@@ -204,7 +212,7 @@ template <> inline float compose_mantissa<float , uint8_t>(const uint8_t m, cons
 
 
 template <class T, class S_EXP_T>
-inline T compose_sign_exponent(const S_EXP_T s_exp, const T src_fp, const int move_up) {
+AONFP_HOST_DEVICE inline T compose_sign_exponent(const S_EXP_T s_exp, const T src_fp, const int move_up) {
 	const auto exponent_bitstring = get_exponent_bitstring(s_exp);
 	auto dst_exponent = static_cast<typename bitstring_t<T>::type>(0);
 	if (exponent_bitstring) {
--- include/aonfp/detail/standard_fp.hpp
+++ include/aonfp/detail/standard_fp.hpp
@@ -1,30 +1,38 @@
 #ifndef __AONFP_DETAIL_STANDARDFP_HPP__
 #define __AONFP_DETAIL_STANDARDFP_HPP__
+
+#ifndef AONFP_HOST_DEVICE
+ #if defined(__CUDA_ARCH__)
+  #define AONFP_HOST_DEVICE __device__ __host__
+ #else
+  #define AONFP_HOST_DEVICE
+ #endif
+#endif
 #include <cmath>
 #include <cstdint>
 
 namespace aonfp {
 namespace detail {
 namespace standard_fp {
-// If all people in the world used C++14 or later, these functions could be `template constexpr`.
+// If all people in the world used C++14 or later, these functions could be `template AONFP_HOST_DEVICE constexpr`.
 template <class T>
-constexpr unsigned get_exponent_size();
-template <> constexpr unsigned get_exponent_size<float >() {return 8 ;}
-template <> constexpr unsigned get_exponent_size<double>() {return 11;}
+AONFP_HOST_DEVICE constexpr unsigned get_exponent_size();
+template <> AONFP_HOST_DEVICE constexpr unsigned get_exponent_size<float >() {return 8 ;}
+template <> AONFP_HOST_DEVICE constexpr unsigned get_exponent_size<double>() {return 11;}
 
 template <class T>
-constexpr unsigned get_mantissa_size();
-template <> constexpr unsigned get_mantissa_size<float >() {return 23;}
-template <> constexpr unsigned get_mantissa_size<double>() {return 52;}
+AONFP_HOST_DEVICE constexpr unsigned get_mantissa_size();
+template <> AONFP_HOST_DEVICE constexpr unsigned get_mantissa_size<float >() {return 23;}
+template <> AONFP_HOST_DEVICE constexpr unsigned get_mantissa_size<double>() {return 52;}
 
 template <class T>
-constexpr T get_nan(const T original_fp) {
+AONFP_HOST_DEVICE constexpr T get_nan(const T original_fp) {
 	// TODO: sign
 	return NAN;
 }
 
 template <class T>
-constexpr T get_infinity(const T original_fp) {
+AONFP_HOST_DEVICE constexpr T get_infinity(const T original_fp) {
 	// TODO: sign
 	return INFINITY;
 }
