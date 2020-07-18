3e206db38ae61836c1f798a97f72738e9a039602
mutsuki <mutsuki@momo86.net>
2020-07-18 10:41:23 +0900

    Add exceptional process for zero exponent in composition

--- aonfp/detail/detail.hpp
+++ aonfp/detail/detail.hpp
@@ -196,17 +196,21 @@ template <> inline float compose_mantissa<float , uint8_t>(const uint8_t m, cons
 	return *reinterpret_cast<const float*>(&full_bitstring);
 }
 
+
 template <class T, class S_EXP_T>
 inline T compose_sign_exponent(const S_EXP_T s_exp, const T src_fp, const int move_up) {
 	const auto exponent_bitstring = get_exponent_bitstring(s_exp);
-	const auto exponent = static_cast<typename std::make_signed<S_EXP_T>::type>(static_cast<typename std::make_signed<S_EXP_T>::type>(exponent_bitstring) - get_default_exponent_bias(sizeof(S_EXP_T) * 8 - 1) + move_up);
-	if (exponent >= get_max_exponent(standard_fp::get_exponent_size<T>())) {
-		return standard_fp::get_infinity<T>(src_fp);
+	auto dst_exponent = static_cast<typename bitstring_t<T>::type>(0);
+	if (exponent_bitstring) {
+		const auto exponent = static_cast<typename std::make_signed<S_EXP_T>::type>(static_cast<typename std::make_signed<S_EXP_T>::type>(exponent_bitstring) - get_default_exponent_bias(sizeof(S_EXP_T) * 8 - 1) + move_up);
+		if (exponent >= get_max_exponent(standard_fp::get_exponent_size<T>())) {
+			return standard_fp::get_infinity<T>(src_fp);
+		}
+		if (exponent <= get_min_exponent(standard_fp::get_exponent_size<T>())) {
+			return static_cast<T>(0);
+		}
+		dst_exponent = static_cast<typename bitstring_t<T>::type>(exponent + get_default_exponent_bias(standard_fp::get_exponent_size<T>()));
 	}
-	if (exponent <= get_min_exponent(standard_fp::get_exponent_size<T>())) {
-		return static_cast<T>(0);
-	}
-	const auto dst_exponent = static_cast<typename bitstring_t<T>::type>(exponent + get_default_exponent_bias(standard_fp::get_exponent_size<T>()));
 	const auto dst_exponent_bitstring = dst_exponent << standard_fp::get_mantissa_size<T>();
 
 	const auto dst_sign_bitstring = static_cast<typename bitstring_t<T>::type>(get_sign_bitstring(s_exp) >> (sizeof(S_EXP_T) * 8 - 1)) << (sizeof(T) * 8 - 1);
