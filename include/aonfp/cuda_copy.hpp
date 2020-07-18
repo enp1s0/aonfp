895b7457a9aad7c3ef71cfaf0e70b878754b7a7b
mutsuki <mutsuki@momo86.net>
2020-07-18 15:16:55 +0900

    Create cuda_copy.hpp

---
+++ include/aonfp/cuda_copy.hpp
@@ -0,0 +1,12 @@
+#ifndef __AONFP_CUDA_COPY_HPP__
+#define __AONFP_CUDA_COPY_HPP__
+namespace aonfp {
+namespace cuda {
+template <class T, class S_EXP_T, class MANTISSA_T>
+int copy_to_device(T* const dst_ptr, const S_EXP_T* const s_exp_ptr, const MANTISSA_T* const mantissa_ptr);
+
+template <class S_EXP_T, class MANTISSA_T, class T>
+int copy_to_host(S_EXP_T* const s_exp_ptr, MANTISSA_T* const mantissa_ptr, const T* const dst_ptr);
+} // namespace cuda
+} // namespace aonfp
+#endif
