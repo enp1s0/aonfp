8603151c28b295fac8dbe17ff76c62e042acf4d4
mutsuki <mutsuki@momo86.net>
2020-07-20 10:59:36 +0900

    Add `mul_compute_t`

--- include/aonfp/detail/operators.hpp
+++ include/aonfp/detail/operators.hpp
@@ -1,4 +1,14 @@
 #ifndef __AONFP_DETAIL_OPERATORS_HPP__
 #define __AONFP_DETAIL_OPERATORS_HPP__
 #include "detail.hpp"
+namespace aonfp {
+namespace detail {
+template <class RESULT_T>
+struct mul_compute_t {using type = RESULT_T;};
+template <> struct mul_compute_t<uint32_t> {using type = uint64_t;};
+template <> struct mul_compute_t<uint16_t> {using type = uint32_t;};
+template <> struct mul_compute_t<uint8_t > {using type = uint16_t;};
+
+} // namespace detail
+} // namespace aonfp
 #endif
