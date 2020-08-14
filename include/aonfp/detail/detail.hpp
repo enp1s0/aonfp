9a24ce34f3eeb38f4d4ab1ce52b04154d6a69b9d
mutsuki <mutsuki@momo86.net>
2020-08-14 15:36:56 +0900

    Add operator= to `aonfp_uint128_t`

--- include/aonfp/detail/detail.hpp
+++ include/aonfp/detail/detail.hpp
@@ -24,6 +24,11 @@ enum uo_flow_t {
 
 struct aonfp_uint128_t {
 	uint64_t x[2];
+	aonfp_uint128_t operator=(const uint64_t v) {
+		x[0] = v;
+		x[1] = 0;
+		return *this;
+	}
 };
 
 template <class T>
