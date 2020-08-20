32946057b7d8b0b5e29acc9bcf6e0dc77aa6600a
mutsuki <mutsuki@momo86.net>
2020-08-20 19:32:47 +0900

    Add `decompose_mantissa` for float

--- include/aonfp/detail/compose_q.hpp
+++ include/aonfp/detail/compose_q.hpp
@@ -49,6 +49,44 @@ template <> AONFP_HOST_DEVICE inline uint8_t decompose_mantissa<uint8_t>(const d
 	move_up = (mantissa_bs_a & 0x10000000000000lu) >> 52;
 	return (mantissa_bs_a >> 45) | (static_cast<uint8_t>(sign) << 7);
 }
+
+template <class T>
+AONFP_HOST_DEVICE inline T decompose_mantissa(const float v, int& move_up);
+template <> AONFP_HOST_DEVICE inline uint64_t decompose_mantissa<uint64_t>(const float v, int& move_up) {
+	move_up = 0;
+	const auto bitstrings = *reinterpret_cast<const uint32_t*>(&v);
+	const auto sign = (*reinterpret_cast<const uint32_t*>(&v)) >> 31;
+
+	const auto mantissa = static_cast<uint64_t>(bitstrings & 0x7fffff) << (32 + 9 - 1);
+	return mantissa | (static_cast<uint64_t>(sign) << 63);
+}
+
+template <> AONFP_HOST_DEVICE inline uint32_t decompose_mantissa<uint32_t>(const float v, int& move_up) {
+	move_up = 0;
+	const auto bitstrings = *reinterpret_cast<const uint32_t*>(&v);
+	const auto sign = (*reinterpret_cast<const uint32_t*>(&v)) >> 31;
+
+	const auto mantissa = (bitstrings & 0x7fffff) << (9 - 1);
+	return mantissa | (sign << 31);
+}
+
+template <> AONFP_HOST_DEVICE inline uint16_t decompose_mantissa<uint16_t>(const float v, int& move_up) {
+	const auto mantissa_bs = (*reinterpret_cast<const uint32_t*>(&v)) & 0x7fffff;
+	const auto sign = (*reinterpret_cast<const uint32_t*>(&v)) >> 31;
+	const auto r_s = (mantissa_bs & 0x40) << 1;
+	const auto mantissa_bs_a = mantissa_bs + r_s;
+	move_up = (mantissa_bs_a & 0x800000) >> 23;
+	return (mantissa_bs_a >> 8) | (static_cast<uint16_t>(sign) << 15);
+}
+
+template <> AONFP_HOST_DEVICE inline uint8_t decompose_mantissa<uint8_t>(const float v, int& move_up) {
+	const auto mantissa_bs = (*reinterpret_cast<const uint32_t*>(&v)) & 0x7fffff;
+	const auto sign = (*reinterpret_cast<const uint32_t*>(&v)) >> 31;
+	const auto r_s = (mantissa_bs & 0x8000) << 1;
+	const auto mantissa_bs_a = mantissa_bs + r_s;
+	move_up = (mantissa_bs_a & 0x800000) >> 23;
+	return (mantissa_bs_a >> 14) | (static_cast<uint8_t>(sign) << 7);
+}
 } // namespace detail
 } // namespace aonfp
 
