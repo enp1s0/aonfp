161c0fe05bbecf31dc5790d30dc9cef2b60e9883
mutsuki <mutsuki@momo86.net>
2020-09-16 19:49:11 +0900

    Update to use macro.hpp

--- include/aonfp/detail/compose_q.hpp
+++ include/aonfp/detail/compose_q.hpp
@@ -1,15 +1,9 @@
 #ifndef __AONFP_DETAIL_COMPOSE_Q_HPP__
 #define __AONFP_DETAIL_COMPOSE_Q_HPP__
-#ifndef AONFP_HOST_DEVICE
- #if defined(__CUDA_ARCH__)
-  #define AONFP_HOST_DEVICE __device__ __host__
- #else
-  #define AONFP_HOST_DEVICE
- #endif
-#endif
 
 #include "detail.hpp"
 #include "standard_fp.hpp"
+#include "macro.hpp"
 
 namespace aonfp {
 namespace detail {
